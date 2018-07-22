c1edc241fee227ee19374e39f57a13e73c3bf7b6
chenxiankang <wwwxiankang@163.com>
2018-07-22 23:00:35 +0800

    快速排序

---
+++ algorithm/sort/quick_sort.hpp
@@ -0,0 +1,40 @@
+
+
+template<class T>
+int partial(T* data, int q, int r) {
+    int k = data[r];
+    int j=q-1;
+    for(int i=q; i <= r-1; i++) {
+        if(data[i]<=k){
+            j++;
+            T temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+
+    T temp = data[j+1];
+    data[j+1] = k;
+    data[r] = temp;
+    return j+1;
+}
+
+template<class T>
+int randomPartial(T* data, int q, int r) {
+    //加入随机选取主元
+    int random = q + (rand() % (r - q + 1));
+    T temp = data[random];
+    data[random] = data[r];
+    data[r] = temp;
+
+    return partial(data, q, r);
+}
+
+template<class T>
+void quickSort(T* data, int q, int r) {
+    if(q<r){
+        int m = randomPartial(data, q, r);
+        quickSort(data, q, m-1);
+        quickSort(data, m+1, r);
+    }
+}
--- test/test_sort.cpp
+++ test/test_sort.cpp
@@ -1,16 +1,17 @@
 #include "test_sort.hpp"
 #include "heap_sort.hpp"
 #include "i_task.hpp"
+#include "quick_sort.hpp"
 
 template<class T>
-class SortTask: public ITask {
+class HeapSortTask: public ITask {
 public:
-    SortTask(int length) : length_(length) {
+    HeapSortTask(int length) : length_(length) {
         data_ = new T[length_];
         for(int i=0; i<length_; i++)
             data_[i] = rand() % 100;
     }
-    ~SortTask() {
+    ~HeapSortTask() {
         delete data_;
         data_ = nullptr;
     }
@@ -33,12 +34,43 @@ public:
         }
     }
 private:
-    SortTask();
+    HeapSortTask();
     T* data_;
     int length_;
 };
 
+template<class T>
+class QuickSortTask: public ITask {
+public:
+    QuickSortTask(int length) : length_(length) {
+        data_ = new T[length_];
+        for(int i=0; i<length_; i++)
+            data_[i] = rand() % 100;
+    }
+    ~QuickSortTask() {
+        delete data_;
+        data_ = nullptr;
+    }
+
+    void run() {
+        std::cout<<"data"<<std::endl;
+        for(int i=0; i<length_; i++){
+            std::cout<< data_[i] <<" ";
+        }
+        
+        std::cout<<std::endl<<"sort by quick sort"<<std::endl;
+        quickSort(data_, 0, length_-1);
+        for(int i=0; i<length_; i++){
+            std::cout<< data_[i] <<" ";
+        }
+    }
+private:
+    QuickSortTask();
+    T* data_;
+    int length_;
+};
 
 TestSort::TestSort() {
-    addTask(std::unique_ptr<ITask>(new SortTask<int>(300)));
+    addTask(std::unique_ptr<ITask>(new HeapSortTask<int>(300)));
+    addTask(std::unique_ptr<ITask>(new QuickSortTask<int>(300)));
 }
